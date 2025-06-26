918e28ebcaa70a2779a73acb027055f6d0fa91ab
Halim <halimputra0701@gmail.com>
2025-06-26 21:25:40 +0700

    refactor(config): update database config

--- src/app/config/database.py
+++ src/app/config/database.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL", "postgresql://user:password@localhost:5432/pest"
+    "DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/pest"
 )
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
